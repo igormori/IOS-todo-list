95b6ef8e363280d5f7dec1f059d9ae19215888cd
Igor Kenji Mori <101045762@clc330-017m.local>
2019-03-14 10:39:59 -0400

    Initial Commit

---
+++ GBCTodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GBCTodoList
+//
+//  Created by Igor Kenji Mori on 2019-03-14.
+//  Copyright © 2019 Igor Kenji Mori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
